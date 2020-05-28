3f61d37ba6da8a2422c62d22a36f0b956cc671ba
Yadira Tamarit <ystamaritq@gmail.com>
2020-05-28 18:54:27 -0500

    added comments to the function browseDestination, added the event to the search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,17 @@
 
 // Functions
 
+/**
+ * As a User I can browse vacation destinations.
+ * When I click or enter the search button I browse the city
+ */
 function browseDestination() {
 	// code goes here
 }
 
 // On Document Ready (events)
+
+//When I click or enter the search button I browse the city
 $(document).ready(function () {
 	$(".search-button").on("click", browseDestination);
 });
