9796b259e835ec5da22f33c7626c8cda57319790
steveo9219 <stephen.guzman92@gmail.com>
2020-05-29 14:15:05 -0500

    added the query URL for mapbox also the ajax to recieve that response

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,5 +22,13 @@ $(document).ready(function () {
 	$(".top-destinations").on("click", browseTopDestinations);
 });
 
-loadInfo;
+//upon click and value given. inputs the city they would like to vacation to and the cityValue
+//would be entered into the URL and a specific query could be made
 var mapBoxPoi = `https://api.mapbox.com/geocoding/v5/mapbox.places/${cityValue}.json?types=poi&access_token=pk.eyJ1Ijoic3RldmVvOTIxOSIsImEiOiJja2FpbGJtcjYwMjg4MnpxdXVxNHdhaTltIn0.7ggPMksLsnum5sjGqnC4gQ`;
+
+$.ajax({
+	url: mapBoxPoi,
+	method: "GET",
+}).then(function (response) {
+	console.log(response);
+});
